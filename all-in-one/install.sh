c8d129f3f0887b0d0c2a56bf1aa26f380ff58815
Matt Hansen <mahans@microsoft.com>
2021-02-12 17:18:19 -0500

    install script

--- all-in-one/install.sh
+++ all-in-one/install.sh
@@ -1,5 +1,11 @@
-apt install docker
-apt install docker-compose
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+
+sudo apt install docker -y
+sudo apt install docker-compose -y
 
 wget https://raw.githubusercontent.com/matthansen0/azure-openemr/main/all-in-one/docker-compose.yml
 
