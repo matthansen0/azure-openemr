448667142b4ba2691ff919bf6bd017ff720576d1
Matt Hansen <mahans@microsoft.com>
2021-02-14 07:04:10 -0500

    script

--- all-in-one/install.sh
+++ all-in-one/install.sh
@@ -1,3 +1,4 @@
+# Install Prerequisites 
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -7,6 +8,13 @@ sudo apt update
 sudo apt install docker -y
 sudo apt install docker-compose -y
 
+# Download and run sample docker compose file
 wget https://raw.githubusercontent.com/matthansen0/azure-openemr/dev/all-in-one/docker-compose.yml
-
 docker-compose up -d
+
+# Checking Web Service Status
+while [ "$status" != 0 ]
+do
+    status=$(timeout 2 bash -c "</dev/tcp/127.0.0.1/80"; echo $?)
+    sleep 1
+done
