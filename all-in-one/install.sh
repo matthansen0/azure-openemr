8f12e5862e63e24a066131355415700b76782d42
Matt Hansen <mahans@microsoft.com>
2021-02-14 07:47:55 -0500

    script

--- all-in-one/install.sh
+++ all-in-one/install.sh
@@ -14,8 +14,8 @@ wget https://raw.githubusercontent.com/matthansen0/azure-openemr/dev/all-in-one/
 docker-compose up -d
 
 # Checking Web Service Status
-while [ "$?" != 0 ]
+while [ "$status" != 0 ]
 do
-    timeout 2 bash -c "</dev/tcp/127.0.0.1/80"
+    status=$(timeout 2 bash -c "</dev/tcp/127.0.0.1/80")
     sleep 1
 done
