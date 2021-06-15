9385cf4977b15dabc57a41babb8b3c2548a4bfd2
Matt Hansen <mahans@microsoft.com>
2021-06-15 07:03:26 -0400

    6.1.0 update

--- all-in-one/install.sh
+++ all-in-one/install.sh
@@ -4,6 +4,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
+sudo apt upgrade -y
 
 sudo apt install docker -y
 sudo apt install docker-compose -y
